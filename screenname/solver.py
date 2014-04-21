e48253c82325fd10ed7dbbc26e777357cc05f35d
Peixiang xu <peixiangxu@gmail.com>
2014-04-21 22:18:42 +0800

    change name

--- screenname/solver.py
+++ screenname/solver.py
@@ -3,7 +3,7 @@
 
 def solve_it():
     # return a string that is the screen name you would like
-    return 'I_R a-N00B'
+    return 'eagle'
 
 if __name__ == '__main__':
     print 'This script submits the screen name: ' + solve_it() + '\n'
