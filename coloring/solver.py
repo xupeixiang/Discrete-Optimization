e03c69e23487c844cfc1d9711d1cc1453d1e1611
Peixiang xu <peixiangxu@gmail.com>
2014-04-26 00:29:40 +0800

    point 38

--- coloring/solver.py
+++ coloring/solver.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import heapq
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
@@ -14,7 +13,7 @@ def solve_it(input_data):
 
     edges = []
     neighbors = [[] for i in range(node_count)]
-    neighbor_colors = [[-1] for i in range(node_count)]
+    neighbor_colors = [set() for i in range(node_count)]
     colors = [0] * node_count
 
     for i in range(1, edge_count + 1):
@@ -25,32 +24,29 @@ def solve_it(input_data):
         neighbors[int(parts[1])].append(int(parts[0]))
 
     def get_color(neighbor_color):
-        for i, color in enumerate(neighbor_color):
-            if color > i - 1:
-                return i - 1
-        return neighbor_color[-1] + 1
-
-    def ordered_insert(neighbor_color, color):
-        loc = 1
-        for i, c in enumerate(neighbor_color):
-            if c < color:
-                loc = i
+        sorted_colors = sorted(neighbor_color)
+        c = 0
+        for color in sorted_colors:
+            if color == c:
+                c += 1
             else:
                 break
-        neighbor_color.insert(loc + 1, color)
+        return c
 
-    for i in range(node_count):
+    neighbors_num = [(i,len(neighbor)) for i,neighbor in enumerate(neighbors)]
+    orders = sorted(neighbors_num, key = lambda num:num[1], reverse = True)
+
+    for i, _ in orders:
         color = get_color(neighbor_colors[i])
         colors[i] = color
         for neighbor in neighbors[i]:
-            if color not in neighbor_colors[neighbor]:
-                ordered_insert(neighbor_colors[neighbor], color)
-                #print i, color, neighbor, neighbor_colors[neighbor]
+            neighbor_colors[neighbor].add(color)
+        #print i, color, neighbors[i], neighbor_colors[i]
 
     # check
-    for v1,v2 in edges:
-        if colors[v1] == colors[v2]:
-            print 'Error: colors of %s = colors of %s.' %(v1, v2)
+    #for v1,v2 in edges:
+    #    if colors[v1] == colors[v2]:
+    #        print 'Error: colors of %s = colors of %s.' %(v1, v2)
 
     # prepare the solution in the specified output format
     output_data = str(len(set(colors))) + ' ' + str(1) + '\n'
