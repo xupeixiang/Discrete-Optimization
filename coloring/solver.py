655751b68ef7cea79be2a8bb0f98d3d4ab1693f9
Peixiang xu <peixiangxu@gmail.com>
2014-04-26 19:32:53 +0800

    improved with swap

--- coloring/solver.py
+++ coloring/solver.py
@@ -49,43 +49,61 @@ def solve_it(input_data):
         colors[i] = color
         for neighbor in neighbors[i]:
             neighbor_colors[neighbor].add(color)
-    
+    # print 'orginal count: %d' %len(set(colors))
+    changed = True
     # check optimal
-    for i in range(node_count):
-        neighbor_color = neighbor_colors[i]
-        if max(neighbor_color) > len(neighbor_color):
-            print i, ','.join(map(str, neighbor_color))
+    while(changed):
+        changed = False
+        for i in range(node_count):
+            neighbor_color = neighbor_colors[i]
+            if max(neighbor_color) > len(neighbor_color):
+                # may replace the max
+                candidate_color_small =  get_color(list(neighbor_color) + [colors[i]])
+                v_c_pairs = [(neighbor, colors[neighbor]) for neighbor in neighbors[i]]
+                v_big = (max(v_c_pairs, key = lambda p: p[1]))[0]
+                v_color_same = -1
+                for color in range(candidate_color_small):
+                    replaced = True
+                    color_same_new_colors = {}
 
-    # swap
-    #swap_candidates = copy.copy(edges)
-    #while(len(swap_candidates) > 0):
-    #    v1, v2 = swap_candidates.pop()
-    #    color1, color2 = (colors[v1], colors[v2])
-    #    color_small, v_small, color_big, v_big = (color1, v1, color2, v2) if color1 < color2 else (color2, v2, color1, v1)
-    #    all_neighbored_small = True
-    #    for neighbor in neighbors[v_big]:
-    #        if color_small not in neighbor_colors[neighbor]:
-    #            all_neighbored_small = False
-    #            break
-    #    if all_neighbored_small:
-    #        colors[v_big] = color_small
-    #        for neighbor in neighbors[v_big]:
-    #            neighbor_colors[neighbor] = get_neighbor_colors(neighbor)
+                    original_color_i = colors[i]
+                    original_color_big = colors[v_big]
+                    colors[i] = candidate_color_small
 
-    #        neighbor_colors[v_small].add(color_small)
-    #        neighbor_colors[v_small].remove(color_big)
-    #        neighbor_colors[v_big].remove(color_small)
-    #        colors[v_small] = get_color(neighbor_colors[v_small])
-    #        neighbor_colors[v_big].add(colors[v_small])
+                    for neighbor_big in neighbors[v_big]:
+                        if colors[neighbor_big] == color and neighbor_big != i:
+                            colors[v_big] = color
+                            new_neighbor_colors = get_neighbor_colors(neighbor_big)
+                            candidate_color_same = get_color(new_neighbor_colors)
+                            if candidate_color_same >= colors[v_big]:
+                                replaced = False
+                                break
+                            else:
+                                color_same_new_colors[neighbor_big] = candidate_color_same
 
-    #        for neighbor in neighbors[v_small]:
-    #            neighbor_colors[neighbor] = get_neighbor_colors(neighbor)
-    #            swap_candidates.append((v_small, neighbor))
+                    colors[i] = original_color_i
+                    colors[v_big] = original_color_big
+                    if replaced and color_same_new_colors:
+                        changed = True
+                        # print '%d: %d -> %d, %d: %d -> %d, ' % (i, colors[i], candidate_color_small,
+                        #         v_big, colors[v_big], color) + ','.join([str(neighbor_big) + ': ' + str(color) + ' -> ' + str(candidate_color_same)
+                        #             for neighbor_big, candidate_color_same in color_same_new_colors.items()])
+                        # update neighbors colors
+                        need_update_vertex_set = neighbors[i].union(neighbors[v_big])
+                        colors[i] = candidate_color_small
+                        colors[v_big] = color
+                        for v_color_same in color_same_new_colors:
+                            colors[v_color_same] = color_same_new_colors[v_color_same]
+                            need_update_vertex_set = need_update_vertex_set.union(neighbors[v_color_same])
+
+                        for v in need_update_vertex_set:
+                            neighbor_colors[v] = get_neighbor_colors(v)
+                        break
 
     # check
-    #for v1,v2 in edges:
-    #    if colors[v1] == colors[v2]:
-    #        print 'Error: colors of %s = colors of %s.' %(v1, v2)
+    for v1,v2 in edges:
+        if colors[v1] == colors[v2]:
+            print 'Error: colors of %s = colors of %s.' %(v1, v2)
 
     # prepare the solution in the specified output format
     output_data = str(len(set(colors))) + ' ' + str(1) + '\n'
