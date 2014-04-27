f1770bbfd9352ac483cd522b3a1918deaf52a4fe
Peixiang xu <peixiangxu@gmail.com>
2014-04-27 13:19:48 +0800

    a mode optimal solution, but sucks in performance

--- coloring/solver.py
+++ coloring/solver.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import copy
+
+candidate_colors = set()
+colors = []
+best_colors = []
+neighbor_colors = []
+baseline = 0
 
 def solve_it(input_data):
-    # Modify this code to run your optimization algorithm
+    global best_colors
+    global baseline
+    global neighbor_colors
+    global colors
 
     # parse the input
     lines = input_data.split('\n')
@@ -15,6 +25,8 @@ def solve_it(input_data):
     neighbors = [set() for i in range(node_count)]
     neighbor_colors = [set() for i in range(node_count)]
     colors = [0] * node_count
+    best_colors = []
+    baseline = node_count
 
     for i in range(1, edge_count + 1):
         line = lines[i]
@@ -23,89 +35,53 @@ def solve_it(input_data):
         neighbors[int(parts[0])].add(int(parts[1]))
         neighbors[int(parts[1])].add(int(parts[0]))
 
-    def get_color(neighbor_color):
-        sorted_colors = sorted(neighbor_color)
-        c = 0
-        for color in sorted_colors:
-            if color == c:
-                c += 1
-            else:
-                break
-        return c
-
-    def get_neighbor_colors(vertex):
+    def get_neighbor_colors(vertex, ordered = True):
         neighbor_colors_v = set()
         for neighbor in neighbors[vertex]:
-            neighbor_colors_v.add(colors[neighbor])
-
+            if not ordered or neighbor < vertex:
+                neighbor_colors_v.add(colors[neighbor])
         return neighbor_colors_v
 
-    neighbors_num = [(i,len(neighbor)) for i,neighbor in enumerate(neighbors)]
-    orders = sorted(neighbors_num, key = lambda num:num[1], reverse = True)
+    # use dp
+    def dfs(index, color):
+        global candidate_colors
+        global best_colors
+        global baseline
+        global neighbor_colors
+        global colors
 
-    for i, _ in orders:
-        color = get_color(neighbor_colors[i])
-        colors[i] = color
-        for neighbor in neighbors[i]:
-            neighbor_colors[neighbor].add(color)
-
-    print 'original: %d' % len(set(colors))
-    changed = True
-    # check optimal
-    while(changed):
-        changed = False
-        for i in range(node_count):
-            neighbor_color = neighbor_colors[i]
-            if max(neighbor_color) > len(neighbor_color):
-                # may replace the max
-                candidate_color_small =  get_color(list(neighbor_color) + [colors[i]])
-                v_c_pairs = [(neighbor, colors[neighbor]) for neighbor in neighbors[i]]
-                v_big = (max(v_c_pairs, key = lambda p: p[1]))[0]
-                v_color_same = -1
-                for color in range(colors[v_big]):
-                    if color == candidate_color_small: continue
-                    replaced = True
-                    color_same_new_colors = {}
-
-                    original_color_i = colors[i]
-                    original_color_big = colors[v_big]
-                    colors[i] = candidate_color_small
-
-                    for neighbor_big in neighbors[v_big]:
-                        if colors[neighbor_big] == color and neighbor_big != i:
-                            colors[v_big] = color
-                            new_neighbor_colors = get_neighbor_colors(neighbor_big)
-                            candidate_color_same = get_color(new_neighbor_colors)
-                            if candidate_color_same >= colors[v_big]:
-                                replaced = False
-                                break
-                            else:
-                                color_same_new_colors[neighbor_big] = candidate_color_same
-
-                    colors[i] = original_color_i
-                    colors[v_big] = original_color_big
-                    if replaced and color_same_new_colors:
-                        changed = True
-                        # update neighbors colors
-                        need_update_vertex_set = neighbors[i].union(neighbors[v_big])
-                        colors[i] = candidate_color_small
-                        colors[v_big] = color
-                        for v_color_same in color_same_new_colors:
-                            colors[v_color_same] = color_same_new_colors[v_color_same]
-                            need_update_vertex_set = need_update_vertex_set.union(neighbors[v_color_same])
-
-                        for v in need_update_vertex_set:
-                            neighbor_colors[v] = get_neighbor_colors(v)
-                        break
-
+        if index >= node_count: # won't happen internally
+            return
+        already_color_count = len(candidate_colors)
+        neighbor_colors[index] = get_neighbor_colors(index)
+        # print index, color, neighbor_colors[index], candidate_colors
+        if color in neighbor_colors[index] or (color not in candidate_colors and
+                already_color_count >= baseline - 1) or already_color_count >= baseline: # can't be better
+            return
+        else:
+            candidate_colors.add(color)
+            colors[index] = color
+            cnt_color_count = len(candidate_colors)
+            if index == node_count - 1:
+                if cnt_color_count < baseline: # better
+                    print 'Baseline: %d -> %d' % (baseline, cnt_color_count)
+                    best_colors = copy.copy(colors)
+                    baseline = cnt_color_count
+                return
+            for color in (list(candidate_colors) + [len(candidate_colors)]):
+                dfs(index + 1, color)
+                # restore index + 1 status
+                colors[index + 1:] = [-1] * (node_count - index - 1)
+                candidate_colors = set([color for color in colors if color > -1])
+    dfs(0, 0)
     # check
     for v1,v2 in edges:
-        if colors[v1] == colors[v2]:
+        if best_colors[v1] == best_colors[v2]:
             print 'Error: colors of %s = colors of %s.' %(v1, v2)
 
     # prepare the solution in the specified output format
-    output_data = str(len(set(colors))) + ' ' + str(1) + '\n'
-    output_data += ' '.join(map(str, colors))
+    output_data = str(len(set(best_colors))) + ' ' + str(1) + '\n'
+    output_data += ' '.join(map(str, best_colors))
 
     return output_data
 
